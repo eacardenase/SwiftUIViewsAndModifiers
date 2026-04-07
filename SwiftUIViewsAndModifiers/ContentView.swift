c7703e3fccab5b3a65400b05dae3b0cd386c4ef1
Edwin Cardenas <ecardenas@lwolf.com>
2026-04-06 20:50:18 -0500

    feat: buttons and labels

--- SwiftUIViewsAndModifiers/ContentView.swift
+++ SwiftUIViewsAndModifiers/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        //        Button("Hello world") {
+        //            print(type(of: self.body))
+        //        }
+        //        .font(.largeTitle)
+        //        .foregroundStyle(.white)
+        //        .frame(width: 200, height: 200)
+        //        .background(.red)
+        //        .clipShape(.rect(cornerRadius: 10))
+
+        Button {
+            print(type(of: self.body))
+        } label: {
+            Text("Hello world")
+                .font(.largeTitle)
+                .foregroundStyle(.white)
+                .frame(width: 200, height: 200)
+                .background(.red)
+                .clipShape(.rect(cornerRadius: 8))
         }
-        .padding()
     }
 }
 
