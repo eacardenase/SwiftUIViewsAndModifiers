7ea83acf05e24b70826ba4858c71ed3b3cc9fd32
Edwin Cardenas <ecardenas@lwolf.com>
2026-04-06 19:13:08 -0500

    Initial Commit

---
+++ SwiftUIViewsAndModifiers/SwiftUIViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewsAndModifiersApp.swift
+//  SwiftUIViewsAndModifiers
+//
+//  Created by Edwin Cardenas on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
