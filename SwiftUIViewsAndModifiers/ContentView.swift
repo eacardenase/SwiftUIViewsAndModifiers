0e275e2e381cdba5deb500e7ae5b9af1db00ea56
Edwin Cardenas <ecardenas@lwolf.com>
2026-04-06 20:56:56 -0500

    feat: stacking up vide modifiers

--- SwiftUIViewsAndModifiers/ContentView.swift
+++ SwiftUIViewsAndModifiers/ContentView.swift
@@ -9,25 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        //        Button("Hello world") {
-        //            print(type(of: self.body))
-        //        }
-        //        .font(.largeTitle)
-        //        .foregroundStyle(.white)
-        //        .frame(width: 200, height: 200)
-        //        .background(.red)
-        //        .clipShape(.rect(cornerRadius: 10))
-
-        Button {
-            print(type(of: self.body))
-        } label: {
-            Text("Hello world")
-                .font(.largeTitle)
-                .foregroundStyle(.white)
-                .frame(width: 200, height: 200)
-                .background(.red)
-                .clipShape(.rect(cornerRadius: 8))
-        }
+        Text("Hello, world!")
+            .padding()
+            .background(.red)
+            .clipShape(.buttonBorder)
+            .padding()
+            .background(.green)
+            .clipShape(.buttonBorder)
+            .padding()
+            .background(.blue)
+            .clipShape(.buttonBorder)
+            .padding()
+            .background(.yellow)
+            .clipShape(.buttonBorder)
     }
 }
 
