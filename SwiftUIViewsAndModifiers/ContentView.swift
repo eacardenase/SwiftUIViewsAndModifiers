2d10fa32c09c1e6a4730f20b51245e7a16c9469a
Edwin Cardenas <eacardenase@gmail.com>
2026-04-14 09:55:58 -0500

    feat: custom containers

--- SwiftUIViewsAndModifiers/ContentView.swift
+++ SwiftUIViewsAndModifiers/ContentView.swift
@@ -7,70 +7,34 @@
 
 import SwiftUI
 
-// MARK: - CustomTitle
-
-struct CustomTitle: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundStyle(.white)
-            .padding()
-            .background(.blue)
-            .clipShape(.rect(cornerRadius: 8))
-    }
-}
-
-extension View {
-    func customTitleStyle() -> some View {
-        modifier(CustomTitle())
-    }
-}
-
-// MARK: - Watermark
-
-struct Watermark: ViewModifier {
-    let text: String
-
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-
-            Text(text)
-                .font(.caption)
-                .foregroundStyle(.white)
-                .padding(5)
-                .background(.black.mix(with: .white, by: 0.5))
-        }
-    }
-}
-
-extension View {
-    func watermarked(with text: String) -> some View {
-        modifier(Watermark(text: text))
-    }
-}
-
-// MARK: - CapsuleText
-
-struct CapsuleText: View {
-    let text: String
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
 
     var body: some View {
-        Text(text)
-            .customTitleStyle()
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 10) {
-            CapsuleText(text: "First")
-
-            CapsuleText(text: "Second")
+        GridStack(rows: 4, columns: 4) { row, col in
+            VStack {
+                Image(systemName: "\(row * 4 + col).circle")
+                Text("R\(row) C\(col)")
+            }
+            .frame(width: 50, height: 50)
+            .background(.purple)
         }
-        .frame(width: 300, height: 300)
-        .border(.black)
-        .watermarked(with: "Crafting Hands")
     }
 }
 
